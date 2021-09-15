9ea50acb52c9c40b7f203da87c4215e96dd13071
Reizzend <86672653+Reizzend@users.noreply.github.com>
2021-09-15 19:10:00 +0300

    Update dz1.py
    2

--- dz1.py
+++ dz1.py
@@ -2,12 +2,12 @@ print("Любое число, которые Вы введёте в конечн
 
 num=int(input("Веедите число"))
 
-mult = num*2
+mult = num * 2
 print("Число умножено на 2", mult)
-summ = mult+8
+summ = mult + 8
 print("К числу прибавили 8", summ)
-sep = summ/2
+sep = summ / 2
 print("Число разделили на 2", sep)
-res = sep-num
+res = sep - num
 print("Из числа отняли исходное значение", res)
 print("Результат = ", int(res))
